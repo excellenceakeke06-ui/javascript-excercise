d40c336475b0a42167dd749761716ae062010292
excellenceakeke06-ui <excellenceakeke06@gmail.com>
2026-01-01 15:50:07 +0100

    Add files via upload

--- excercise 1.js
+++ excercise 1.js
@@ -1,1 +1,42 @@
-hmmmmmmmmjnmbjn kjm n
+let firstname = "Excellence"
+let lastname = "Akeke"
+let age = 19
+let fullname = lastname +'  '+ firstname
+intro = "My name is" + " " + fullname + "  " + "and i am" +" " + age + " " + "years old"
+console.log(intro)
+
+
+let a = 15
+let b = 8
+let c = a + b
+let d = a - b
+let e = a * b
+let f = a / b
+
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+
+let temperature = 22
+if(temperature < 20){
+    console.log("It's  cold ")
+} else {
+    console.log("It's warm")
+}
+
+
+let price = 50
+let discount = 10
+let finalPrice = price - discount
+console.log(finalPrice)
+
+let number = 7
+
+if(number>0){
+    console.log("The number is positive")
+} else if  (number < 0){
+    console.log("The number is negative")
+} else {
+    console.log("The number is zero")
+}
