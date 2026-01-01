a6c64f8e873bca5ff47fdedbca803b83e4fbf512
excellenceakeke06-ui <excellenceakeke06@gmail.com>
2026-01-01 15:51:26 +0100

    Add files via upload

---
+++ excersice 3.js
@@ -0,0 +1,43 @@
+let mark = 85 
+let A = 90-100
+let B = 80-89
+let C = 70-79
+let D = 60-69
+console.log(mark<60 ? "D above" : "F")
+
+
+let length = 5
+let width = 10
+let area = length * width
+let perimeter = length + width
+console.log(area)
+console.log(perimeter)
+
+
+var year = 2024
+if(year/4){
+    console.log("leap year")
+}else{"not leap year"}
+
+
+let number = 47
+if (number / 3){
+  console.log( "just by 3")
+} else if (number/5){
+    console.log("just by 5")
+} else {" by neither"}
+
+
+let math = 78
+let physic = 85
+let biology =92
+let compilation = math+physic+biology
+average = compilation /3
+if(average>=90){
+    console.log("Excellent")
+}else if (average>=75){
+    console.log("good")
+}else if (average>=60){
+    console.log("average")
+}else{"need improvement"}
+
