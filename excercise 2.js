fc0143bb246d3c6c56cf0bb8f689fc0c20750478
excellenceakeke06-ui <excellenceakeke06@gmail.com>
2026-01-01 15:50:55 +0100

    Add files via upload

---
+++ excercise 2.js
@@ -0,0 +1,28 @@
+let score = 75 
+if(score >= 50){
+    console.log("You passed the exam")
+}else {
+    console.log("You failed the exam")
+}
+
+let a = 12
+let b = 15
+console.log(a > b ? "a is greater than b": "a is less than b")
+
+let age = 16
+if(age>=18){
+    console.log("can vote")
+}else { 
+    console.log("cannot vote")
+}
+
+let isweekend = true
+if(isweekend=true){
+    console.log("no work today")
+}else {
+    console.log("there is work today")
+}
+
+
+let number = 24
+console.log(number % 2 ==0 ? "it is even":"it is odd")
